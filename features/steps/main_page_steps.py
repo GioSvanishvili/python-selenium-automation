56fe53047f08419d57faef37088d74572a63f08c
Zurab Svanishvili <svanishvili1799@gmail.com>
2024-07-22 17:06:50 -0400

    Add product_details files and implemente waits

--- features/steps/main_page_steps.py
+++ features/steps/main_page_steps.py
@@ -1,20 +1,20 @@
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from behave import given, when, then
+from selenium.webdriver.support import expected_conditions as EC
+
 from time import sleep
 
 
 @given('Open target.com')
 def open_target(context):
-    context.driver.get('https://target.com/')
-    sleep(4)
+    context.driver.get('https://target.com')
 
 
 @when('Search for {item}')
 def search_for_item(context, item):
     context.driver.find_element(By.ID, 'search').send_keys(f'{item}')
-    context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/Search/SearchButton']").click()
-    sleep(6)
+    context.driver.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-test='@web/Search/SearchButton']"))).click()
 
 
 @when('Click on Target Circle')
@@ -24,17 +24,14 @@ def click_target_circle(context):
 
 @when('Click on Sign in button')
 def main_sign_in(context):
-    context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/AccountLink'] span").click()
-    sleep(4)
+    context.driver.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-test='@web/AccountLink'] span"))).click()
 
 
 @when('Click on Sign in in navigation panel')
 def navigational_sign_in(context):
-    context.driver.find_element(By.CSS_SELECTOR, "[data-test='accountNav-signIn'] span").click()
-    sleep(4)
+    context.driver.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-test='accountNav-signIn'] span"))).click()
 
 
 @when('Click on Cart icon')
 def click_cart_icon(context):
-    context.driver.find_element(By.CSS_SELECTOR, "[href='/icons/Cart.svg#Cart']").click()
-    sleep(4)
+    context.driver.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[href='/icons/Cart.svg#Cart']"))).click()
--- features/steps/search_steps.py
+++ features/steps/search_steps.py
@@ -1,24 +1,23 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then
 from time import sleep
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 @when("Click 'Add to cart' on the first item")
 def add_to_cart(context):
-    context.driver.find_element(By.CSS_SELECTOR, "[data-test='@web/site-top-of-funnel/ProductCardWrapper'] [data-test='chooseOptionsButton']").click()
-    sleep(3)
+    context.driver.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-test='@web/site-top-of-funnel/ProductCardWrapper'] [data-test='chooseOptionsButton']"))).click()
 
 
 @when("Click 'Add to cart' button on the navigational panel")
 def add_to_cart_navigational(context):
-    context.driver.find_element(By.CSS_SELECTOR, "[data-test='orderPickupButton']").click()
-    sleep(3)
+    context.driver.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-test='orderPickupButton']"))).click()
 
 
 @when("Click 'View cart & check out' button")
 def view_cart(context):
-    context.driver.find_element(By.CSS_SELECTOR, "[href='/cart']").click()
-    sleep(5)
+    context.driver.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[href='/cart']"))).click()
 
 
 
